794c1807afe030a7c3ffb9cef6b4c3d8dd6490b2
root <root@localhost.localdomain>
2026-03-05 00:06:13 +0100

    update texte payement 4 final

--- Controllers/PaymentController.cs
+++ Controllers/PaymentController.cs
@@ -58,9 +58,9 @@ namespace CrudDemo.Controllers
                             {
                                 Name = "Abonnement Mensuel - Accès Illimité",
                                 Description = 
-                                "• 1 heure de visio pour définir votre parcours.\n\n" +
-                                "• Garantie satisfait ou remboursé.\n\n" +
-                                "• Ajout de contenu personnalisé selon vos demandes.\n\n" +
+                                "• 1 heure de visio pour définir votre parcours.\n" +
+                                "• Garantie satisfait ou remboursé.\n" +
+                                "• Ajout de contenu personnalisé selon vos demandes.\n" +
                                 "• Une équipe professionnelle toujours à vos côtés pour vous accompagner tout au long de votre parcours de formation.",
                             },
                             UnitAmount = 1900, // 19 EUR en centimes
